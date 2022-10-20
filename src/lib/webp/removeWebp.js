9ee9643713b67d736d7dd0954f679990683ffff4
zhuwei-ones <zhuwei@ones.cn>
2022-10-20 18:29:01 +0800

    feat: removeWebp 方法实现

--- src/lib/webp/removeWebp.js
+++ src/lib/webp/removeWebp.js
@@ -1,3 +1,14 @@
-function removeWebp() {}
+import { removeSync } from 'fs-extra';
+import { getWebpTransformPath, log } from '../utils';
+
+function removeWebp(imgPath) {
+  const { pluginOptions: { entryPath, outputPath } } = this.options;
+
+  const webpPath = getWebpTransformPath(imgPath, { entryPath, outputPath });
+
+  removeSync(webpPath);
+
+  log(`${webpPath} remove`);
+}
 
 export default removeWebp;
