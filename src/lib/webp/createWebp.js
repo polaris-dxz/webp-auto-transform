8e1052b76369477ce1cb5fc960c432fb9c79c11c
zhuwei-ones <zhuwei@ones.cn>
2022-10-20 18:28:55 +0800

    feat: createWebp 方法实现

--- src/lib/webp/createWebp.js
+++ src/lib/webp/createWebp.js
@@ -1,3 +1,15 @@
-function createWebp() {}
+import { execFileSync } from 'child_process';
+import cwebp from 'cwebp-bin';
+import { getWebpTransformPath, log } from '../utils';
+
+function createWebp(imgPath) {
+  const { cwebpOptions, pluginOptions: { entryPath, outputPath } } = this.options;
+
+  const webpPath = getWebpTransformPath(imgPath, { entryPath, outputPath });
+
+  execFileSync(cwebp, [...cwebpOptions, imgPath, '-o', webpPath]);
+
+  log(`${webpPath} create`);
+}
 
 export default createWebp;
