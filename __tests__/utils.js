382722d0f34d5ff553dd6be74c6a38266ff698c0
zhuwei-ones <zhuwei@ones.cn>
2022-10-21 18:17:59 +0800

    test: 解决测试用例错误

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -3,193 +3,250 @@ const {
   isDir,
   verifyOptions,
   setDefaultPluginOptions,
-  getWebpTransformPath
-} = require('../src/lib/utils');
+  getWebpTransformPath,
+  getCwebpOptions,
+  getAbsolutePath,
+} = require("../src/lib/utils");
+
+const path = require("path");
+
+const entryPath = "./example/images";
+const outputPath = "./example/images-webp-test";
+const childPath = "./a";
+const demoPng = `${entryPath}/xxxx.png"`;
+const demoWebp = `${outputPath}/xxxx.webp`;
+
+const CWD = process.cwd();
+
+const currentCwd = "/Users/Project/webp-auto-transform";
+
+const entryAbPath = path.resolve(CWD, entryPath);
+const outputAbPath = path.resolve(CWD, outputPath);
+const childAbPath = path.resolve(CWD, childPath);
+const webpPath = path.resolve(CWD, demoWebp);
+const demoPngPath = path.resolve(CWD, demoPng);
 
 const defaultEntry = {
-  entryPath: './example/images'
+  entryPath: entryPath,
 };
 
 const defaultOptions = {
   ...defaultEntry,
+  outputPath: outputPath,
   biggerWebpDelete: true,
   customList: [],
-  outputPath: './example/images-webp',
   quality: 75,
-  webpExistReplace: false
+  webpExistReplace: false,
+  quiet: true,
+};
+
+const resultOptions = {
+  ...defaultOptions,
+  entryPath: entryAbPath,
+  outputPath:getAbsolutePath("./example/images-webp")
 };
 
 const errorList = {
-  entryPath: new Error('entryPath 不是一个有效的路径'),
-  outputPath: new Error('outputPath 不是一个有效的路径'),
-  customList: new Error('customList 必须是一个数组'),
-  customItem: new Error('customList 子项不是有效值'),
-  quality: new Error('quality 必须是0-100'),
-  biggerWebpDelete: new Error('biggerWebpDelete 只能是布尔值'),
-  webpExistReplace: new Error('webpExistReplace 只能是布尔值')
+  entryPath: new Error("entryPath 不是一个有效的路径"),
+  outputPath: new Error("outputPath 不是一个有效的路径"),
+  customList: new Error("customList 必须是一个数组"),
+  customItem: new Error("customList 子项不是有效值"),
+  quality: new Error("quality 必须是0-100"),
+  biggerWebpDelete: new Error("biggerWebpDelete 只能是布尔值"),
+  webpExistReplace: new Error("webpExistReplace 只能是布尔值"),
 };
 
-test('Test isDir', () => {
+test("Test isDir", () => {
   expect(isDir()).toBe(false);
-  expect(isDir('./aaaa')).toBe(false);
-  expect(isDir('./src')).toBe(true);
+  expect(isDir("./aaaa")).toBe(false);
+  expect(isDir("./src")).toBe(true);
 });
 
-test('Test WebpPath',()=>{
+test("Test WebpPath", () => {
+  expect(
+    getWebpTransformPath(demoPng, {
+      entryPath: entryPath,
+      outputPath: outputPath,
+    })
+  ).toBe(webpPath);
+  expect(
+    getWebpTransformPath(demoPngPath, {
+      entryPath: getAbsolutePath(entryPath),
+      outputPath: getAbsolutePath(outputPath),
+    })
+  ).toBe(webpPath);
+});
 
-  expect(getWebpTransformPath("./example/images/xxxx.png",{
-    entryPath:"./example/images",
-    outputPath:"./example/images-webp"
-  })).toBe("./example/images-webp/xxxx.webp")
-})
-
-test('Test Error Options', () => {
+test("Test Error Options", () => {
   expect(() => {
     verifyOptions();
   }).toThrow(errorList.entryPath);
 
   expect(() => {
     verifyOptions({
-      entryPath: true
+      entryPath: true,
     });
   }).toThrow(errorList.entryPath);
 
   expect(() => {
     verifyOptions({
       ...defaultEntry,
-      outputPath: true
+      outputPath: true,
     });
   }).toThrow(errorList.outputErr);
 
   expect(() => {
     verifyOptions({
       ...defaultEntry,
-      customList: true
+      customList: true,
     });
   }).toThrow(errorList.customList);
 
   expect(() => {
     verifyOptions({
       ...defaultEntry,
-      customList: [true]
+      entryPath: entryAbPath,
+      customList: [true],
     });
   }).toThrow(errorList.customItem);
 
   expect(() => {
     verifyOptions({
-      entryPath: './src',
-      quality: true
+      entryPath: entryAbPath,
+      quality: true,
     });
   }).toThrow(errorList.quality);
 
   expect(() => {
     verifyOptions({
       ...defaultEntry,
-      quality: 101
+      entryPath: entryAbPath,
+      quality: 101,
     });
   }).toThrow(errorList.quality);
 
   expect(() => {
     verifyOptions({
       ...defaultEntry,
-      quality: -1
+      entryPath: entryAbPath,
+      quality: -1,
     });
   }).toThrow(errorList.quality);
 
   expect(() => {
     verifyOptions({
       ...defaultEntry,
-      biggerWebpDelete: 1
+      entryPath: entryAbPath,
+      biggerWebpDelete: 1,
     });
   }).toThrow(errorList.biggerWebpDelete);
 
   expect(() => {
     verifyOptions({
       ...defaultEntry,
-      webpExistReplace: 2
+      entryPath: entryAbPath,
+      webpExistReplace: 2,
     });
   }).toThrow(errorList.webpExistReplace);
 });
 
-test('Test Correct Options ', () => {
+test("Test Correct Options ", () => {
   expect(
     verifyOptions({
       ...defaultEntry,
-      outputPath: './dist',
+      outputPath: "./dist",
       customList: [
         {
-          path: './src/a',
-          quality: 90
-        }
+          path: "./src/a",
+          quality: 90,
+        },
       ],
       quality: 10,
       biggerWebpDelete: true,
-      webpExistReplace: false
+      webpExistReplace: false,
     })
   ).toBe(undefined);
 });
 
-test('Test Default Options', () => {
-  expect(setDefaultPluginOptions(defaultEntry)).toStrictEqual(defaultOptions);
+test("Test Default Options", () => {
+  expect(setDefaultPluginOptions(defaultEntry)).toStrictEqual({
+    ...resultOptions
+  });
   expect(
     setDefaultPluginOptions({
       ...defaultEntry,
-
       customList: [
         {
-          path: './a'
-        }
-      ]
+          path: childPath,
+        },
+      ],
     })
-  ).toStrictEqual(defaultOptions);
+  ).toStrictEqual(resultOptions);
 
   expect(
     setDefaultPluginOptions({
       ...defaultEntry,
       customList: [
         {
-          path: './a',
-          quality: 80
-        }
-      ]
+          path: childPath,
+          quality: 80,
+        },
+      ],
     })
   ).toStrictEqual({
-    ...defaultOptions,
+    ...resultOptions,
     customList: [
       {
-        path: './a',
-        quality: 80
-      }
-    ]
+        path: childAbPath,
+        quality: 80,
+      },
+    ],
   });
 
   expect(
     setDefaultPluginOptions({
       ...defaultEntry,
       biggerWebpDelete: false,
-      webpExistReplace: true
+      webpExistReplace: true,
     })
   ).toStrictEqual({
-    ...defaultOptions,
+    ...resultOptions,
     biggerWebpDelete: false,
-    webpExistReplace: true
+    webpExistReplace: true,
   });
 });
 
-test('Test Options Format', () => {
+test("Test Options Format", () => {
   expect(() => {
     getCurrentOptions();
   }).toThrow(errorList.entryPath);
 
-  expect(getCurrentOptions(defaultOptions)).toStrictEqual({
-    pluginOptions: defaultOptions,
-    cwebpOptions: ['-q', 75]
+  const options = {
+    ...defaultOptions,
+    entryPath: path.resolve(currentCwd, entryPath),
+    outputPath: path.resolve(currentCwd, outputPath),
+  };
+
+  expect(
+    getCurrentOptions({ ...options, near_lossless: 79, z: 9 })
+  ).toStrictEqual({
+    pluginOptions: delete options.quiet && options,
+    cwebpOptions: {
+      quality: 75,
+      quiet: true,
+      near_lossless: 79,
+      z: 9,
+    },
   });
 
   expect(
-    getCurrentOptions({ ...defaultOptions, near_lossless: 79, z: 9 })
-  ).toStrictEqual({
-    pluginOptions: defaultOptions,
-    cwebpOptions: ['-q', 75, '-near_lossless', 79, '-z', 9]
-  });
+    getCwebpOptions({
+      quality: 75,
+      quiet: true,
+      near_lossless: 79,
+      z: 9,
+      o: "./index.js",
+      bbbb: false,
+    })
+  ).toStrictEqual(["-q", 75, "-quiet", "-near_lossless", 79, "-z", 9]);
 });
--- __tests__/webp.js
+++ __tests__/webp.js
@@ -1,55 +1,64 @@
 import createWebp from "../src/lib/webp/createWebp";
-import fs from 'fs-extra'
+import fs from "fs-extra";
 import removeWebp from "../src/lib/webp/removeWebp";
+import path from "path";
+import { getAbsolutePath } from "../src/lib/utils";
 
-const entryPath = "./example/images"
-const outputPath = "./example/images-webp-test"
-const img = `${entryPath}/mail_contact_us.png`
-const webp = `${outputPath}/mail_contact_us.webp`
+const entryPath = "./example/images";
+const outputPath = "./example/images-webp-test";
 
-fs.ensureDirSync(outputPath)
+const currentCwd = process.cwd();
+
+const entryAbPath = path.resolve(currentCwd, entryPath);
+const outputAbPath = path.resolve(currentCwd, outputPath);
+
+const img = path.resolve(entryAbPath, "mail_contact_us.png");
+const webp = path.resolve(outputAbPath, "mail_contact_us.webp");
+
+console.log("img",img);
+console.log("webp",webp);
+
+fs.ensureDirSync(outputAbPath);
 
 const context = {
-    options:{
-        pluginOptions:{entryPath,outputPath},
-        cwebpOptions:[]
-    }
-}
+  options: {
+    // 这里是方法单元测试，不能传入相对路径，因为没有经过里面的方法处理，处理之后是绝对路径
+    pluginOptions: { entryPath: getAbsolutePath(entryPath), outputPath: getAbsolutePath(outputPath) },
+    cwebpOptions: {},
+  },
+};
 
 const contextWithCwebParams = {
-    options:{
-        pluginOptions:{entryPath,outputPath,quality:75},
-        cwebpOptions:[]
-    }
-}
-
+  options: {
+    pluginOptions: {
+      entryPath: getAbsolutePath(entryPath),
+      outputPath: getAbsolutePath(outputPath),
+      quality: 75,
+    },
+    cwebpOptions: {},
+  },
+};
 
 test("Test webp ", () => {
+  fs.removeSync(webp);
 
-    fs.removeSync(webp)
+  createWebp.call(context, img);
 
-    createWebp.call(context,img)
+  expect(fs.existsSync(webp)).toBe(true);
 
-    expect(fs.existsSync(webp)).toBe(true)
+  removeWebp.call(context, img);
 
-    removeWebp.call(context,img)
-
-    expect(fs.existsSync(webp)).toBe(false)
-
+  expect(fs.existsSync(webp)).toBe(false);
 });
-
 
 test("Test webp defined cwebp params", () => {
+  fs.removeSync(webp);
 
-    fs.removeSync(webp)
+  createWebp.call(contextWithCwebParams, img);
 
-    createWebp.call(contextWithCwebParams,img)
+  expect(fs.existsSync(webp)).toBe(true);
 
-    expect(fs.existsSync(webp)).toBe(true)
+  removeWebp.call(contextWithCwebParams, img);
 
-    removeWebp.call(contextWithCwebParams,img)
-
-    expect(fs.existsSync(webp)).toBe(false)
-
+  expect(fs.existsSync(webp)).toBe(false);
 });
-
